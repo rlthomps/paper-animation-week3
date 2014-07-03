2c17c4317b773e4604b2a987fb9151486e099f72
rlthomps <rlthomps@rlthomps-macbookair2.roam.corp.google.com>
2014-07-03 14:25:50 -0700

    Initial Commit

---
+++ paper-animation-week3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  paper-animation-week3
+//
+//  Created by Robert Thompson on 7/3/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ paper-animation-week3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  paper-animation-week3
+//
+//  Created by Robert Thompson on 7/3/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
